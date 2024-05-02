e42670112f70a9baf2367cfc3a3d4936062c4c4b
Raj Kumar <67369763+Raj-Kumar13@users.noreply.github.com>
2024-05-02 22:15:22 +0530

    Feature/develop (#2)
    * TEST: added files
    
    * TEST: added wdio.conf.ts files

---
+++ source/config/capabilities.ts
@@ -0,0 +1,16 @@
+//Local RUN CONFIG
+
+export const BROWSER_CONFIG_LOCAL = [{
+    maxInstances: 10,
+    browserName: 'chrome',
+    'goog:chromeOptions':{
+        args: [
+            '--headless',
+            '--ignore-certificate-errors',
+            '--disable-gpu',
+            '--disable-software-rasterizer',
+            '--disable-notification',
+            '--window-size=1920,1080'
+        ]
+    }
+}];
