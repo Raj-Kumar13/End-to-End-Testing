fdf57a412b7e4c905c6cb9a0f5835d57aeb749fb
Raj Kumar <67369763+Raj-Kumar13@users.noreply.github.com>
2024-05-02 23:06:43 +0530

    Feature/develop (#3)
    * TEST: added files
    
    * TEST: added wdio.conf.ts files
    
    * TEST: First Demo test Case

--- source/config/capabilities.ts
+++ source/config/capabilities.ts
@@ -5,7 +5,9 @@ export const BROWSER_CONFIG_LOCAL = [{
     browserName: 'chrome',
     'goog:chromeOptions':{
         args: [
-            '--headless',
+
+           // '--headless',
+
             '--ignore-certificate-errors',
             '--disable-gpu',
             '--disable-software-rasterizer',
